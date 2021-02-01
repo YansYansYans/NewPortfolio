8230e7c8684ba7e52820fe7ae47058514286de25
Yannis <yan95100@gmail.com>
2021-02-01 14:07:04 +0100

    Updates

---
+++ precache-manifest.552b1e8dcc0df3da955cd20304087075.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ba9abbac728a11456f486aa5ee190d",
+    "url": "/NewPortfolio/index.html"
+  },
+  {
+    "revision": "ab7dc970e2b4e800a9a9",
+    "url": "/NewPortfolio/static/css/main.d65ef29a.chunk.css"
+  },
+  {
+    "revision": "46076d429093d9b09ceb",
+    "url": "/NewPortfolio/static/js/2.eca573fb.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/NewPortfolio/static/js/2.eca573fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7dc970e2b4e800a9a9",
+    "url": "/NewPortfolio/static/js/main.23b62a57.chunk.js"
+  },
+  {
+    "revision": "4823778c5cfc2e340d15",
+    "url": "/NewPortfolio/static/js/runtime-main.9a212ffe.js"
+  },
+  {
+    "revision": "9f59c15df3c9f88331b12892c365c663",
+    "url": "/NewPortfolio/static/media/arrowBottom.9f59c15d.png"
+  },
+  {
+    "revision": "48d5721fd84844a884eace45e1b7a526",
+    "url": "/NewPortfolio/static/media/back.48d5721f.svg"
+  },
+  {
+    "revision": "a598962f08531a3eb71dd47e09471abe",
+    "url": "/NewPortfolio/static/media/ball.a598962f.png"
+  },
+  {
+    "revision": "50811a779cc6108911ec881c95e9522f",
+    "url": "/NewPortfolio/static/media/burger.50811a77.png"
+  },
+  {
+    "revision": "ef32a30df7400618a87a6d5106395d9f",
+    "url": "/NewPortfolio/static/media/communication.ef32a30d.png"
+  },
+  {
+    "revision": "1319c44745ec05bfd517f6eaf2ed790e",
+    "url": "/NewPortfolio/static/media/concept.1319c447.png"
+  },
+  {
+    "revision": "fa038467188572e6039fac8562c7cd81",
+    "url": "/NewPortfolio/static/media/cursor.fa038467.png"
+  },
+  {
+    "revision": "330686b69c9fcb6c91f697dcd35f967c",
+    "url": "/NewPortfolio/static/media/dalil.330686b6.png"
+  },
+  {
+    "revision": "8881da4e811e609b1d460b9a79c98c76",
+    "url": "/NewPortfolio/static/media/design.8881da4e.png"
+  },
+  {
+    "revision": "861e356049e4363ea5a95126d732cf9e",
+    "url": "/NewPortfolio/static/media/dev.861e3560.png"
+  },
+  {
+    "revision": "671e60987d677a05f3ad947cb2a06d3f",
+    "url": "/NewPortfolio/static/media/familit.671e6098.png"
+  },
+  {
+    "revision": "6a8e3d70e730f0a8ee5e0d3afe404311",
+    "url": "/NewPortfolio/static/media/figure.6a8e3d70.svg"
+  },
+  {
+    "revision": "ea76484a0e1edc1b1540187765dd6e1c",
+    "url": "/NewPortfolio/static/media/grille.ea76484a.png"
+  },
+  {
+    "revision": "9784773bc36ece2ac35cb5b16855b9fc",
+    "url": "/NewPortfolio/static/media/hand.9784773b.png"
+  },
+  {
+    "revision": "695a8477564b12e4dfe3f9e302173cc8",
+    "url": "/NewPortfolio/static/media/hand2.695a8477.png"
+  },
+  {
+    "revision": "fea44a4e7d6ec0015808b8babb2bcc84",
+    "url": "/NewPortfolio/static/media/logo1.fea44a4e.png"
+  },
+  {
+    "revision": "4ba7fe2f1f111226bc7bfb8063a420ad",
+    "url": "/NewPortfolio/static/media/nebularts.4ba7fe2f.png"
+  },
+  {
+    "revision": "87a84fdc2757f95ee674567985c75d04",
+    "url": "/NewPortfolio/static/media/pc.87a84fdc.png"
+  },
+  {
+    "revision": "14932c74543125545ebe948761956185",
+    "url": "/NewPortfolio/static/media/pen.14932c74.png"
+  },
+  {
+    "revision": "6403f785faf426da4f0b5a19d0a22736",
+    "url": "/NewPortfolio/static/media/phone.6403f785.png"
+  }
+]);
